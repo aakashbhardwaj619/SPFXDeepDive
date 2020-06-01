2307f56a38004d1c9a8f2092f073470c2589dca6
Aakash Bhardwaj <aakashbhardwaj619@gmail.com>
2020-06-01 16:47:52 +0530

    Update DeployPackage.sh

--- scripts/DeployPackage.sh
+++ scripts/DeployPackage.sh
@@ -1,5 +1,4 @@
-echo 'Adding App Package'
-echo "$siteUrl"
-app=$(o365 spo app add --filePath $packagePath --scope sitecollection --appCatalogUrl $siteUrl )
+echo "Adding App Package $packagePath on $siteUrl"
+app=o365 spo app add --filePath $packagePath --scope sitecollection --appCatalogUrl $siteUrl
 echo 'Deploying App'
 o365 spo app deploy --id $app --scope sitecollection --appCatalogUrl $siteUrl
